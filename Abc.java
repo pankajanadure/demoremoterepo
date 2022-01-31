470d7978a0258aed46327b9a7e989988650e6a0e
pankajanadure <pankajanadure@gmail.com>
2022-01-31 10:28:11 +0530

    Created Atoz and modified Abc, Xyz

--- Abc.java
+++ Abc.java
@@ -2,6 +2,9 @@ class Abc
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Everyone");
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("Have a Good Day");
+		}
 	}
 }
--- Xyz.java
+++ Xyz.java
@@ -1,3 +1,7 @@
 public class Xyz
 {
+	public void display()
+	{
+		System.out.println("HELLO");
+	}
 }
